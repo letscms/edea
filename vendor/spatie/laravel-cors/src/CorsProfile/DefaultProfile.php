7dc172203137756c5f4fbb656b79f2afdaff87a9
Jitendra Sharma <jitendra@atxlearning.com>
2018-03-29 21:17:51 +0530

    initial

---
+++ vendor/spatie/laravel-cors/src/CorsProfile/DefaultProfile.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Spatie\Cors\CorsProfile;
+
+class DefaultProfile implements CorsProfile
+{
+    /** Illuminate\Http\Request */
+    protected $request;
+
+    public function setRequest($request)
+    {
+        $this->request = $request;
+    }
+
+    public function allowOrigins(): array
+    {
+        return config('cors.default_profile.allow_origins');
+    }
+
+    public function allowMethods(): array
+    {
+        return config('cors.default_profile.allow_methods');
+    }
+
+    public function allowHeaders(): array
+    {
+        return config('cors.default_profile.allow_headers');
+    }
+
+    public function maxAge(): int
+    {
+        return config('cors.default_profile.max_age');
+    }
+
+    public function addCorsHeaders($response)
+    {
+        return $response
+            ->header('Access-Control-Allow-Origin', $this->allowedOriginsToString());
+    }
+
+    public function addPreflightHeaders($response)
+    {
+        return $response
+            ->header('Access-Control-Allow-Methods', $this->toString($this->allowMethods()))
+            ->header('Access-Control-Allow-Headers', $this->toString($this->allowHeaders()))
+            ->header('Access-Control-Allow-Origin', $this->allowedOriginsToString())
+            ->header('Access-Control-Max-Age', $this->maxAge());
+    }
+
+    public function isAllowed(): bool
+    {
+        if (! in_array($this->request->method(), $this->allowMethods())) {
+            return false;
+        }
+
+        if (in_array('*', $this->allowOrigins())) {
+            return true;
+        }
+
+        return in_array($this->request->header('Origin'), $this->allowOrigins());
+    }
+
+    protected function toString(array $array): string
+    {
+        return implode(', ', $array);
+    }
+
+    protected function allowedOriginsToString(): string
+    {
+        if (! $this->isAllowed()) {
+            return '';
+        }
+
+        if (in_array('*', $this->allowOrigins())) {
+            return '*';
+        }
+
+        return $this->request->header('Origin');
+    }
+}
